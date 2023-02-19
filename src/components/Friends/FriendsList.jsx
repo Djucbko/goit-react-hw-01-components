9e39f80439dc60798eb845e0cc5d463c1176806e
Djucbko <109214332+Djucbko@users.noreply.github.com>
2023-02-19 12:24:23 +0200

    hw1

---
+++ src/components/Friends/FriendsList.jsx
@@ -0,0 +1,20 @@
+import css from './FriendsList.module.css';
+import FriendsListItem from './FriendsListItem';
+
+export default function FriendsList({ friends }) {
+    return (
+        <ul className={css.friendList}>
+            {friends.map(friend => {
+                const { id, isOnline, avatar, name } = friend;
+                return (
+                    <FriendsListItem
+                        key={id}
+                        isOnline={isOnline}
+                        avatar={avatar}
+                        name={name}
+                    />
+                );
+            })}
+        </ul>
+    );
+}
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types';
+import css from './Statistic.module.css';
+import { getRandomHexColor } from '../../utils/index';
+
+export default function PaintingStatistics({ title, stats }) {
+  return (
+    <section className={css.statistics}>
+      {title && <h2 className={css.title}>{title}</h2>}
+
+      <ul className={css.statList}>
+        {stats.map(paintings => {
+          return (
+            <li
+              className={css.item}
+              key={paintings.id}
+              style={{ backgroundColor: getRandomHexColor() }}
+            >
+              <span className={css.label}>{paintings.label}</span>
+              <span className={css.percentage}>{paintings.percentage}%</span>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
+}
+
+PaintingStatistics.propTypes = {
+  title: PropTypes.string,
+  label: PropTypes.string,
+  percentage: PropTypes.number,
+};
